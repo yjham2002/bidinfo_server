296d4b935cbe93c98e756b176c3633d446f96062
univtable <yjham2002@gmail.com>
2016-09-16 05:15:18 +0900

    simple rest

--- web.js
+++ web.js
@@ -1,17 +1,6 @@
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'yjham2016',
-  password : 'gpswpf12',
-});
+var server = require('./server');
+var router = require('./router');
+var requestHandlers = require('./requestHandlers');
+ 
+server.start(router.route, requestHandlers.handle);
 
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log(rows);
-  console.log('The solution is: ', rows[0].solution);
-});
-
-connection.end();

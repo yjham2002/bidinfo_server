84617ccb143f758274d422aed29d98a3a44a4ac0
univtable <yjham2002@gmail.com>
2016-09-18 15:09:06 +0900

    w

--- web.js
+++ web.js
@@ -1,19 +1,9 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
 app.use('/', routes);
 app.use('/users', users);
 
@@ -33,8 +23,6 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {

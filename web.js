cdd5e2b76389114c1328e42852da09efcc27c0e7
univtable <yjham2002@gmail.com>
2016-09-18 15:18:27 +0900

    a

--- web.js
+++ web.js
@@ -1,10 +1,8 @@
 var express = require('express');
-var path = require('path');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-app.set('view engine', 'html');
 
 app.use('/', routes);
 app.use('/users', users);
@@ -13,24 +11,6 @@ app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
-});
-
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
 });
 
 app.listen(8001);

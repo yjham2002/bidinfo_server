e5c007cad2ddd63d7d942081999500f6324e4ca9
univtable <yjham2002@gmail.com>
2016-10-07 20:29:06 +0900

    temporary using

--- routes/users.js
+++ routes/users.js
@@ -17,20 +17,31 @@ connection.connect(function(err) {
     }
 });
 
-router.post('/new', function (req, res){
-    res.write(req.body.id + '(new user)');
-    res.end();
-});
-
 router.get('/:id', function(req, res) {
     res.write(req.params.id);
     res.end();
 });
 
-router.get('/', function(req, res) {
-    var query = connection.query('select * from univtable_member', [], function(err,rows){
+router.post('/new', function(req, res) {
+    var data = {
+        'Uid':req.body.Uid,
+        'Pwd':req.body.Pwd,
+        'Name':req.body.Name,
+        'ExpDate':req.body.ExpDate,
+        'Bdate':req.body.Bdate,
+        'Status':req.body.Status,
+        'Phone':req.body.Phone
+    };
+    var query = connection.query('insert into Bidinfo_user set ?', data, function(err,rows){
         res.json(rows);
-        res.write(req.params.id);
+        console.log(rows);
+    });
+    console.log(query);
+});
+
+router.get('/', function(req, res) {
+    var query = connection.query('select * from Bidinfo_user', [], function(err,rows){
+        res.json(rows);
         console.log(rows);
     });
     console.log(query);

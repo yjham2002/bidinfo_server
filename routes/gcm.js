6263a8328abafcba6a0d328eae7f3369ad7ae518
univtable <yjham2002@gmail.com>
2016-10-11 20:54:27 +0900

    fcm added

--- routes/gcm.js
+++ routes/gcm.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
+var FCM = require('fcm').FCM;
+var fcm = new FCM(apiKey);
 
 var connection = mysql.createConnection({
   host    :'10.0.0.1',
@@ -17,6 +19,35 @@ connection.connect(function(err) {
         console.error(err);
         throw err;
     }
+});
+
+router.post('/send/:id', bodyParser.urlencoded({
+    extended: true
+}), function(req, res) {
+    var apiKey = 'AIzaSyDW9EBF7iPBaRAQHLvlYljQ1OyiFI-6RGs';
+    var message = {
+        registration_id: 'fk7gZNVvmjc:APA91bEdF3O8YyeX_UQCc-44naEk--dwj3MaPiwP4GxUrae_jfKXx7KQbrMms9XFPPvqgpOMvvcj99ajHTFXfIeIn7HS8hRi--9sYX0Ve_agZv0xL9emaHsmmsjJQwoMtYJrDfcnnNAN', // required
+        collapse_key: 'Collapse key',
+        title:'title test',
+        msg:'msg test', 
+        'data.key1': 'value1',
+        'data.key2': 'value2'
+    };
+
+    
+
+    fcm.send(message, function(err, messageId){
+        if (err) {
+            console.log("Something has gone wrong!");
+        } else {
+            console.log("Sent with message ID: ", messageId);
+        }
+    });
+    var query = connection.query('select `Token` from `Bidinfo_GCM` where `mid`=' + req.params.id + ' and `Status` <> 1', [], function(err,rows){
+        res.json(rows);
+        console.log(rows);
+    });
+    console.log(query);
 });
 
 router.get('/:id', function(req, res) {
